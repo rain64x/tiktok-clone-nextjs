d929438f43050036c18d08266f4d71cb0cdea8a2
Mahesh Anand Kadri <mahesh@rafled.com>
2024-08-28 00:13:33 +0530

    Main Page Layout -  Added Post component, added post preview on homepage, function to detect on screen and autoplay video, added share,like buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 "use client"
+import PostMain from "./components/PostMain";
 import MainLayout from "./layouts/MainLayout";
 
 export default function Home() {
   return (
-   <>
-   <MainLayout>
-      <div>
-        HOME
-      </div>
-   </MainLayout>
-   </>
+    <>
+      <MainLayout>
+        <div className="mt-[80px]  w-[calc(100%-90px)] max-w-[690px] ml-auto">
+          <PostMain post={{
+            id: '123',
+            user_id: '456',
+            video_url: '/beach.mp4',
+            text: ' this is some text',
+            created_at: 'date here',
+            profile: {
+              user_id: '7890123456',
+              name: 'John Doe',
+              image: 'https://placehold.co/100',
+            }
+          }}
+          />
+        </div>
+      </MainLayout>
+    </>
   );
 }
